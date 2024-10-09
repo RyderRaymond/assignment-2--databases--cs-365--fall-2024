de1cef5dd73533036efe7a4d5655edbffe3fa71c
Ryder Raymond <117836603+RyderRaymond@users.noreply.github.com>
2024-10-09 19:50:43 -0400

    Updated timestamp to be DATETIME. Begin inserting values.

--- setup.sql
+++ setup.sql
@@ -24,14 +24,14 @@ SET @init_vector = RANDOM_BYTES(16);
 CREATE TABLE IF NOT EXISTS users (
     first_name VARCHAR(128) NOT NULL,
     last_name VARCHAR(128) NOT NULL,
-    id SMALLINT NOT NULL,
+    id SMALLINT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS websites (
     name VARCHAR(128) NOT NULL,
     url VARCHAR(256) NOT NULL,
-    id SMALLINT NOT NULL,
+    id SMALLINT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id)
 );
 
@@ -42,9 +42,28 @@ CREATE TABLE IF NOT EXISTS credentials (
     user_id SMALLINT NOT NULL,  -- Relation to users table. Way to know which user this username and password is associated with.
     site_id SMALLINT NOT NULL,  -- Relation to websites table. Way to know what website this is associated with.
     comment VARCHAR(512),
-    timestamp DATE NOT NULL
+    time_stamp DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 
 
 /* Insert values for the database */
+INSERT INTO users VALUES
+    ('John', 'Smith'),
+    ('Mark', 'Fishbach'),
+    ('Peter', 'Parker'),
+    ('Tony', 'Stark'),
+    ('Chris', 'Evans'),
+    ('Satoru', 'Gojo'),
+    ('Suguru', 'Geto'),
+    ('Roy', 'Vanegas'),
+    ('Carolyn', 'Rosiene'),
+    ('Olusola', 'Agboola');
+
+INSERT INTO websites VALUES
+    ('Youtube', 'https://www.youtube.com/');
+
+
+INSERT INTO credentials VALUES
+    ('jsmith', AES_ENCRYPT('5678JohnSmithIsTheBest62374uerfjncb', @key_str, @init_vector), 'jsmith@outlook.com', 1, 1, 'This will be the most epic youtube account ever')
+
