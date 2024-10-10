cd62cdc2dbb512e79dbbefccc7fc064a2f70c3bd
Ryder Raymond <117836603+RyderRaymond@users.noreply.github.com>
2024-10-09 20:48:19 -0400

    Added more websites

--- setup.sql
+++ setup.sql
@@ -25,14 +25,14 @@ CREATE TABLE IF NOT EXISTS users (
     first_name VARCHAR(128) NOT NULL,
     last_name VARCHAR(128) NOT NULL,
     user_id SMALLINT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE IF NOT EXISTS websites (
-    name VARCHAR(128) NOT NULL,
+    site_name VARCHAR(128) NOT NULL,
     url VARCHAR(256) NOT NULL,
     site_id SMALLINT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (site_id)
 );
 
 CREATE TABLE IF NOT EXISTS credentials (
@@ -50,22 +50,24 @@ CREATE TABLE IF NOT EXISTS credentials (
 /* Insert values for the database */
 INSERT INTO users (first_name, last_name) VALUES
     ('John', 'Smith'),
-    ('Mark', 'Fishbach'),
     ('Peter', 'Parker'),
     ('Tony', 'Stark'),
     ('Chris', 'Evans'),
     ('Satoru', 'Gojo'),
     ('Suguru', 'Geto'),
     ('Roy', 'Vanegas'),
-    ('Carolyn', 'Rosiene'),
-    ('Olusola', 'Agboola');
+    ('Carolyn', 'Rosiene');
 
 
 
-INSERT INTO websites (name, url) VALUES
-    ('Youtube', 'https://www.youtube.com/');
+INSERT INTO websites (site_name, url) VALUES
+    ('Youtube', 'https://www.youtube.com/'),
+    ('GitHub', 'https://github.com/'),
+    ('Crunchyroll', 'https://www.crunchyroll.com/'),
+    ('Uhart blackboard', 'https://blackboard.hartford.edu/');
 
 
 INSERT INTO credentials (username, password, email_address, user_id, site_id, comment) VALUES
-    ('jsmith', AES_ENCRYPT('5678JohnSmithIsTheBest62374uerfjncb', @key_str, @init_vector), 'jsmith@outlook.com', 1, 1, 'This will be the most epic youtube account ever');
-
+    ('jsmith', AES_ENCRYPT('5678JohnSmithIsTheBest62374uerfjncb', @key_str, @init_vector), 'jsmith@outlook.com', 1, 1, 'This will be the most epic youtube account ever'),
+    ('jsmith95', AES_ENCRYPT('4521485bestpassword*290', @key_str, @init_vector), 'jsmith@outlook.com', 1, 2, 'Going to make the best projects on github'),
+    ('not_spiderman2002', AES_ENCRYPT('ThisIsATotallySecurePassword', @key_str, @init_vector), 'pparker@live.com', 2, 1, NULL);
