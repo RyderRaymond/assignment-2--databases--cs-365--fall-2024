71f025cefd720917c9bb6f240696d1c1023aa0d8
Ryder Raymond <117836603+RyderRaymond@users.noreply.github.com>
2024-10-14 13:43:14 -0400

    Changed keystring to 256 bits to remove warnings. Changed credentials to accounts for better description.

--- setup.sql
+++ setup.sql
@@ -15,7 +15,7 @@ use passwords;
 /* Set up encryption */
 
 SET block_encryption_mode = 'aes-256-cbc';
-SET @key_str = UNHEX(SHA2('my secret passphrase', 512));
+SET @key_str = UNHEX(SHA2('my secret passphrase', 256));
 SET @init_vector = RANDOM_BYTES(16);
 
 
@@ -30,12 +30,12 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS websites (
     site_name VARCHAR(128) NOT NULL,
-    url VARCHAR(256) NOT NULL,
+    url VARCHAR(256) UNIQUE NOT NULL,   -- UNIQUE because no two websites can have the same URL
     site_id SMALLINT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (site_id)
 );
 
-CREATE TABLE IF NOT EXISTS credentials (
+CREATE TABLE IF NOT EXISTS accounts (
     username VARCHAR(128) NOT NULL,
     password VARBINARY(512) NOT NULL,
     email_address VARCHAR(128) NOT NULL,
@@ -67,7 +67,7 @@ INSERT INTO websites (site_name, url) VALUES
     ('Uhart blackboard', 'https://blackboard.hartford.edu/');
 
 
-INSERT INTO credentials (username, password, email_address, user_id, site_id, comment) VALUES
+INSERT INTO accounts (username, password, email_address, user_id, site_id, comment) VALUES
     ('jsmith', AES_ENCRYPT('5678JohnSmithIsTheBest62374uerfjncb', @key_str, @init_vector), 'jsmith@outlook.com', 1, 1, 'This will be the most epic youtube account ever'),
     ('jsmith95', AES_ENCRYPT('4521485bestpassword*290', @key_str, @init_vector), 'jsmith@outlook.com', 1, 2, 'Going to make the best projects on github'),
     ('not_spiderman2002', AES_ENCRYPT('ThisIsATotallySecurePassword', @key_str, @init_vector), 'pparker@live.com', 2, 1, NULL);
